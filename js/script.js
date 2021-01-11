3e7d61cd9379b37b5a8af7cb48d2324a6fcb963f
DinaPapsujevica <dina.papsujevica@gmail.com>
2021-01-11 17:01:12 +0100

    Working on modal js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,30 @@ function showSlides() {
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+/* Modal */
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var link = document.getElementById("#openModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+link.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
