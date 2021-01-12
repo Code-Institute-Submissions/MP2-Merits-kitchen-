c7153183bf4f6a22275643312318dc4cb364ff74
DinaPapsujevica <dina.papsujevica@gmail.com>
2021-01-12 16:15:50 +0100

    Writing javascript for email service

---
+++ js/email.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById("sendMessage");
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    btn.value = "Sending...";
+
+    const serviceID = "default_service";
+    const templateID = "template_xp6068j";
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+      () => {
+        btn.value = "Send Email";
+        alert("Sent!");
+      },
+      (err) => {
+        btn.value = "Send Email";
+        alert(JSON.stringify(err));
+      }
+    );
+  });
