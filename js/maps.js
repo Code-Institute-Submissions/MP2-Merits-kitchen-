20073365f571c862de68be35984136f73b267163
DinaPapsujevica <dina.papsujevica@gmail.com>
2021-01-12 14:22:15 +0100

    Adding javascript to maps.js

---
+++ js/maps.js
@@ -0,0 +1,25 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: {
+      lat: 55.64452,
+      lng: 12.63405,
+    },
+  });
+
+  var labels = "A";
+
+  var locations = [{ lat: 55.64452, lng: 12.63405 }];
+
+  var markers = locations.map(function (location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+}
